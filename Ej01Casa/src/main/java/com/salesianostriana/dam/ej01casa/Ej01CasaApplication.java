98ae8df249fd2ee8ab70471de601b3f6b46d3f4d
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2024-04-11 20:38:49 +0200

    Proyecto de prueba en casa

---
+++ Ej01Casa/src/main/java/com/salesianostriana/dam/ej01casa/Ej01CasaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ej01casa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej01CasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej01CasaApplication.class, args);
+	}
+
+}
---
+++ Ej01Casa/src/test/java/com/salesianostriana/dam/ej01casa/Ej01CasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ej01casa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej01CasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
