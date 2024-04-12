a07c089d6d442a3fb2242013afedc75f745f8f8a
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2024-04-12 12:33:05 +0200

    Apuntes programacion

--- Ejemplo01/src/main/java/com/salesianostriana/dam/ejemplo01/ControladorSaludo.java
+++ Ejemplo01/src/main/java/com/salesianostriana/dam/ejemplo01/ControladorSaludo.java
@@ -1,7 +1,9 @@
 package com.salesianostriana.dam.ejemplo01;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControladorSaludo {
@@ -9,8 +11,27 @@ public class ControladorSaludo {
 	@GetMapping({ "/", "welcome" })
 	public String welcome(@RequestParam(name = "nombre", required = false, defaultValue = "Mundo") String nombre, Model model) {
 		model.addAttribute("nombre", nombre);// Incluimos la información en el modelo
-		return "index";
+		return "index";	//Nombre de la página HTML ya que es lo que vamos a devolver.
 	}
 	
 	
+	@GetMapping("/saludo2")
+	public String welcome2(Model model) {
+		model.addAttribute("persona", new Persona("Alvaro", "Castilla Cano"));
+		return "SaludoPersonalizado";
+	}
+	
+	@GetMapping("/saludo3")
+	public String welcome3(Model model) {
+		model.addAttribute("saludo", "Hola Mundo");
+		model.addAttribute("mensaje", "¡Se me está haciendo largo el proyecto final!");
+		model.addAttribute("url", "https://triana.salesianos.edu");
+		
+		return "SaludoYEnlace";
+	}
 }
+
+
+/*La diferencia entre el welcome1 y el welcome2 es que el 2 no hace falta que le pasemos parametros*/
+
+
