bf70a418aea45dbcc6361c81aaadac3e3215ed4d
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2024-04-22 10:01:00 +0200

    Ejemplos en clase de BD

--- Ejemplo01BD/src/main/java/com/salesianostriana/dam/ejemplo01bd/SpanishSaludator.java
+++ Ejemplo01BD/src/main/java/com/salesianostriana/dam/ejemplo01bd/SpanishSaludator.java
@@ -3,6 +3,9 @@ package com.salesianostriana.dam.ejemplo01bd;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Service
 @Primary		//Si en algun momento hay duda en que bean coger, coge este que es el primario.
 public class SpanishSaludator implements Saludator{
@@ -10,7 +13,16 @@ public class SpanishSaludator implements Saludator{
 	@Override
 	public void imprimirSaludo() {
 		System.out.println("Hola a todos los programadores de 1ºDAM");
-		
+	}
+	
+	@PostConstruct
+	void alComenzar() {
+		System.out.println("Método \"alComenzar\" anotado con @PostConstruct");
+	}
+	
+	@PreDestroy
+	void antesDeDestruir() {
+		System.out.println("Método \"antesDeDestruir\" anotado con @PreDestroy");
 	}
 
 }
