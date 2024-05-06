6e99c98ace984ebc27c50f179122914ba44c7477
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2024-05-06 12:41:21 +0200

    Ejemplo de CRUDS sin base de datos

--- EjemploCrudCompleto/src/main/java/com/salesianostriana/dam/ejemplocrudcompleto/model/repos/AlumnoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.ejemplocrudcompleto.model.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.ejemplocrudcompleto.model.Alumno;
-
-public interface AlumnoRepositorio extends JpaRepository<Alumno, Long>{
-
-}
