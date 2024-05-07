1d36a5dc657441ef924ae1f6fe44d19d4b37143a
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2024-05-07 14:19:24 +0200

    Seguridad

---
+++ EjemploSeguridad/src/main/java/com/salesianostriana/dam/ejemploseguridad/EjemploSeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemploseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSeguridadApplication.class, args);
+	}
+
+}
